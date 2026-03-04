shop-pin/urma_learning

commit a7ea7e683ec655f1c9b53540e9a51203c46eb0a2

udma: support alloc and free tid
category: feature
bugzilla: https://gitee.com/openeuler/umdk/issues/ID45DE
CVE: NA

-----------------------------------------------------------

TID (Token ID): Identifies the address space and permission
space to which resources in the UB system belong. Before
registering an application with Segment, you first need
to apply for a tid.

Signed-off-by: Jiahao Zhang <zhangjiahao14@huawei.com>

src/urma/hw/udma/udma_u_ops.c
@@ -14,6 +14,7 @@
 #include "udma_u_jfs.h"
 #include "udma_u_jfr.h"
 #include "udma_u_jetty.h"
+#include "udma_u_tid.h"
 #include "udma_u_db.h"
 #include "udma_u_ctrlq_tp.h"
 #include "udma_u_ops.h"
@@ -52,6 +53,8 @@ static urma_ops_t g_udma_ops = {
 	.import_jetty_ex = udma_u_import_jetty_ex,
 	.import_jfr_ex = udma_u_import_jfr_ex,
 	.bind_jetty_ex = udma_u_bind_jetty_ex,
+	.alloc_token_id = udma_u_alloc_tid,
+	.free_token_id = udma_u_free_tid,
 	.get_async_event = udma_u_get_async_event,
 	.ack_async_event = udma_u_ack_async_event,
 	.post_jfs_wr = udma_u_post_jfs_wr,

src/urma/hw/udma/CMakeLists.txt
@@ -14,6 +14,7 @@ target_include_directories(BETA_UDMA_SO PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/kern
 
 target_link_libraries(BETA_UDMA_SO PRIVATE urma)
 target_link_libraries(BETA_UDMA_SO PRIVATE urma_common)
+target_link_libraries(BETA_UDMA_SO PRIVATE ummu)
 
 set(UDMA_NAME urma-udma)
 set_target_properties(BETA_UDMA_SO PROPERTIES OUTPUT_NAME ${UDMA_NAME})

src/urma/hw/udma/udma_u_common.h
@@ -146,6 +146,11 @@ struct udma_u_jfc {
 	uint32_t cq_shift;
 };
 
+struct udma_u_tid {
+	urma_token_id_t base;
+	uint32_t tid;
+};
+
 struct udma_u_jetty_grp {
 	urma_jetty_grp_t base;
 	uint32_t jetty_cnt;
@@ -309,6 +314,11 @@ static inline uint32_t align_power2(uint32_t n)
 		res++;
 
 	return res;
+}
+
+static inline struct udma_u_tid *to_udma_u_tid(urma_token_id_t *key_id)
+{
+	return container_of(key_id, struct udma_u_tid, base);
 }
 
 static inline struct udma_u_jetty_grp *

src/urma/hw/udma/udma_u_tid.c
@@ -1,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#include <ummu_api.h>
+#include "udma_u_tid.h"
+
+static int udma_exec_alloc_tid_cmd(urma_context_t *ctx, uint32_t tid,
+				   urma_token_id_t *keyid)
+{
+	urma_cmd_udrv_priv_t udata = {};
+	int ret;
+
+	udma_u_set_udata(&udata, &tid, sizeof(tid), NULL, 0);
+	ret = urma_cmd_alloc_token_id(ctx, keyid, &udata);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd alloc tid failed, ret = %d.\n", ret);
+
+	return ret;
+}
+
+static urma_token_id_t *udma_u_alloc_tid_common(urma_context_t *ctx, enum ummu_mapt_mode mapt_mode)
+{
+	struct ummu_tid_attr tid_attr = {.mode = mapt_mode};
+	struct udma_u_tid *udma_tid;
+	uint32_t tid;
+	int ret;
+
+	udma_tid = (struct udma_u_tid *)calloc(1, sizeof(*udma_tid));
+	if (udma_tid == NULL) {
+		UDMA_LOG_ERR("allocate udma tid failed.\n");
+		return NULL;
+	}
+
+	ret = ummu_allocate_tid(&tid_attr, &tid);
+	if (ret != 0) {
+		UDMA_LOG_ERR("ummu allocate tid failed, ret = %d.\n", ret);
+		goto err_ummu_alloc_tid;
+	}
+
+	if (tid > UDMA_MAX_TID) {
+		UDMA_LOG_ERR("ummu allocate tid overflow.\n");
+		goto err_cmd_alloc_key_id;
+	}
+
+	udma_tid->tid = tid;
+	udma_tid->base.token_id = tid << UDMA_TID_SHIFT;
+	ret = udma_exec_alloc_tid_cmd(ctx, udma_tid->base.token_id, &udma_tid->base);
+	if (ret != 0)
+		goto err_cmd_alloc_key_id;
+
+	udma_tid->base.urma_ctx = ctx;
+
+	return &udma_tid->base;
+
+err_cmd_alloc_key_id:
+	ret = ummu_free_tid(tid);
+	if (ret != 0)
+		UDMA_LOG_ERR("ummu free tid failed, ret = %d.\n", ret);
+err_ummu_alloc_tid:
+	free(udma_tid);
+	return NULL;
+}
+
+urma_status_t udma_u_free_tid(urma_token_id_t *tid)
+{
+	struct udma_u_tid *udma_tid = to_udma_u_tid(tid);
+	int ret;
+
+	ret = urma_cmd_free_token_id(tid);
+	if (ret != 0) {
+		UDMA_LOG_ERR("urma cmd free tid failed, ret = %d.\n", ret);
+		return URMA_FAIL;
+	}
+
+	ret = ummu_free_tid(udma_tid->tid);
+	if (ret != 0)
+		UDMA_LOG_ERR("ummu free tid failed, ret = %d.\n", ret);
+
+	free(udma_tid);
+
+	return URMA_SUCCESS;
+}
+
+urma_token_id_t *udma_u_alloc_tid(urma_context_t *ctx)
+{
+	return udma_u_alloc_tid_common(ctx, MAPT_MODE_TABLE);
+}

src/urma/hw/udma/udma_u_tid.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#ifndef __UDMA_U_TID_H__
+#define __UDMA_U_TID_H__
+
+#include "udma_u_common.h"
+
+urma_token_id_t *udma_u_alloc_tid(urma_context_t *ctx);
+urma_status_t udma_u_free_tid(urma_token_id_t *tid);
+
+#endif /* __UDMA_U_TID_H__ */

openeuler-mirror/umdk

commit f2a928c16b6c7209305f266c0b2a4b3aa9609c2f

ub: udma: Support uboe function.
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/umdk/issues/160
CVE: NA

-----------------------------------------------------------

This patch support uboe function.

Fixes: a7ea7e683ec6 ("udma: support alloc and free tid")

Signed-off-by: Wei Qin <qinwei61@huawei.com>

src/urma/hw/udma/udma_u_ctrlq_tp.c
@@ -49,3 +49,47 @@ int udma_u_ctrlq_get_tp_attr(const urma_context_t *ctx, const uint64_t tp_handle
 
 	return ret;
 }
+
+urma_status_t udma_u_get_smac(const urma_context_t *ctx, uint8_t *mac)
+{
+	urma_status_t ret;
+
+	ret = urma_cmd_get_smac(ctx, mac);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd get smac failed, ret = %d.\n", ret);
+
+	return ret;
+}
+
+urma_status_t udma_u_get_dmac(const urma_context_t *ctx, const urma_net_addr_t *net_addr, uint8_t *mac)
+{
+	urma_status_t ret;
+
+	ret = urma_cmd_get_dmac(ctx, net_addr, mac);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd get dmac failed, ret = %d.\n", ret);
+
+	return ret;
+}
+
+urma_status_t udma_u_get_eid_by_ip(const urma_context_t *ctx, const urma_net_addr_t *net_addr, urma_eid_t *eid)
+{
+	urma_status_t ret;
+
+	ret = urma_cmd_get_eid_by_ip(ctx, net_addr, eid);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd get eid by ip failed, ret = %d.\n", ret);
+
+	return ret;
+}
+
+urma_status_t udma_u_get_ip_by_eid(const urma_context_t *ctx, const urma_eid_t *eid, urma_net_addr_t *net_addr)
+{
+	urma_status_t ret;
+
+	ret = urma_cmd_get_ip_by_eid(ctx, eid, net_addr);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd get ip by eid failed, ret = %d.\n", ret);
+
+	return ret;
+}

src/urma/hw/udma/udma_u_ctrlq_tp.h
@@ -11,6 +11,7 @@
 #define __UDMA_U_CTRLQ_TP_H__
 
 #include "udma_u_common.h"
+#include "udma_u_ctl.h"
 
 int udma_u_ctrlq_get_tp_list(urma_context_t *ctx, urma_get_tp_cfg_t *cfg, uint32_t *tp_cnt,
 			     urma_tp_info_t *tp_list);
@@ -21,4 +22,10 @@ int udma_u_ctrlq_get_tp_attr(const urma_context_t *ctx, const uint64_t tp_handle
 int udma_u_ctrlq_set_tp_attr(const urma_context_t *ctx, const uint64_t tp_handle,
 			     const uint8_t tp_attr_cnt, const uint32_t tp_attr_bitmap,
 			     const urma_tp_attr_value_t *tp_attr);
+
+urma_status_t udma_u_get_smac(const urma_context_t *ctx, uint8_t *mac);
+urma_status_t udma_u_get_dmac(const urma_context_t *ctx, const urma_net_addr_t *net_addr, uint8_t *mac);
+urma_status_t udma_u_get_eid_by_ip(const urma_context_t *ctx, const urma_net_addr_t *net_addr, urma_eid_t *eid);
+urma_status_t udma_u_get_ip_by_eid(const urma_context_t *ctx, const urma_eid_t *eid, urma_net_addr_t *net_addr);
+
 #endif /* __UDMA_U_CTRLQ_TP_H__ */

src/urma/hw/udma/udma_u_ops.c
@@ -76,6 +76,10 @@ static urma_ops_t g_udma_ops = {
 	.rearm_jfc = udma_u_rearm_jfc,
 	.wait_jfc = udma_u_wait_jfc,
 	.ack_jfc = udma_u_ack_jfc,
+	.get_eid_by_ip = udma_u_get_eid_by_ip,
+	.get_ip_by_eid = udma_u_get_ip_by_eid,
+	.get_smac = udma_u_get_smac,
+	.get_dmac = udma_u_get_dmac,
 };
 
 static urma_status_t udma_u_init(urma_init_attr_t *conf)