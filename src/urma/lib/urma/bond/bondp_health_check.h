d6a727fdb7131a7a26ef7240006b4b784b040a14
jilei12138 <jilei8@huawei.com>
2026-04-15 17:22:28 +0800

    urma: update health check cr status

--- src/urma/lib/urma/bond/bondp_health_check.h
+++ src/urma/lib/urma/bond/bondp_health_check.h
@@ -35,13 +35,15 @@ void bondp_stop_health_check_thread(void);
 
 int bondp_create_health_check_ctx(bondp_context_t *bond_ctx);
 void bondp_destroy_health_check_ctx(bondp_context_t *bond_ctx);
+int bondp_register_health_check_seg_for_jetty(bondp_context_t *bond_ctx, bondp_comp_t *bdp_jetty);
+void bondp_unregister_health_check_seg_for_jetty(bondp_comp_t *bdp_jetty);
 int bondp_fill_vjetty_health_info(bondp_context_t *bond_ctx, bondp_comp_t *bdp_jetty,
 	urma_bond_seg_info_out_t *health_check_seg, bool *is_health_check_enable);
 int bondp_import_health_check_tseg(bondp_context_t *bdp_ctx, bondp_target_jetty_t *bdp_tjetty,
 	urma_bond_id_info_out_t *rvjetty_info, urma_rjetty_t *rjetty);
 int bondp_unimport_health_check_tseg(bondp_target_jetty_t *bdp_tjetty);
 int bondp_register_health_check_task(bondp_context_t *bdp_ctx, bondp_target_jetty_t *bdp_tjetty, bondp_comp_t *cfg_jetty);
-void bondp_unregister_health_check_task(bondp_target_jetty_t *bdp_tjetty);
+void bondp_unregister_health_check_task(bondp_context_t *bdp_ctx, bondp_target_jetty_t *bdp_tjetty);
 void bondp_health_update_active_idx(bondp_context_t *bdp_ctx, bondp_target_jetty_t *bdp_tjetty,
 	int new_active_idx);
 bool bondp_try_handle_health_check_cr(bondp_context_t *bdp_ctx, int local_idx, urma_cr_t *cr);
