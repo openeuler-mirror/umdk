f2f41ee667125c1cae4f5dedfa583472a6cad2ca
lairuilang <lawrenlang1@gmail.com>
2026-04-24 15:10:59 +0800

    urma:recovery backup link

---
+++ src/urma/lib/urma/bond/bondp_link_recovery.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * Description: Bond provider link recovery declarations
+ */
+
+#ifndef BONDP_LINK_RECOVERY_H
+#define BONDP_LINK_RECOVERY_H
+
+#include "bondp_types.h"
+
+int bondp_rebuild_local_pjetty(bondp_health_task_t *task, int local_idx);
+
+#endif // BONDP_LINK_RECOVERY_H
