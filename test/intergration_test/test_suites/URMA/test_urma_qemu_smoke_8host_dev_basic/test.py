8fd85c9d7545cd6fe8df3c60ffa2d0da6f8d423d
h60062168 <huaiyu6@h-partners.com>
2026-04-25 19:11:51 +0800

    mode urma test_case

--- test/intergration_test/test_suites/URMA/test_urma_qemu_smoke_8host_dev_basic/test.py
+++ test/intergration_test/test_suites/URMA/test_urma_qemu_smoke_8host_dev_basic/test.py
@@ -6,8 +6,8 @@
 """
 
 from itertools import combinations
-import random
 import logging
+import pytest
 from public import UBUSFeature
  
 log = logging.getLogger()
@@ -23,19 +23,25 @@ class Test(UBUSFeature):
         log.info('---------- [ Test teardown ] ----------')
         super(Test, self).teardown()
  
+    @pytest.mark.timeout(1600)
     def test_urma_qemu_smoke_8host_dev_basic(self):
         p_list = []
-        # Pairwise traffic traversal
-        for host1, host2 in combinations(self.host_list, 2):
-            opt = "--ctp "
-            if random.randint(0, 1) == 1:
-                opt += "--tp_aware "
-            p_list.append(self.urma_perftest_one_perf_single_dev(host1, host2, opt=opt))
+        cmd_list = ["read_bw", "write_bw", "send_bw", "read_lat", "write_lat", "send_lat"]
+        mode_list = [
+            "-p 0 --eid_idx 0",
+            "-p 1 --eid_idx 0",
+            "-p 0 --ctp",
+            "-p 1 --ctp",
+            "-p 2 --ctp",
+            "-p 0 --ctp --tp_aware",
+            "-p 1 --ctp --tp_aware",
+            "-p 2 --ctp --tp_aware"
+        ]
 
-        # Randomly generate 20 flows again
-        for _ in range(20):
-            opt = "--ctp "
-            host1, host2 = random.sample(self.host_list, 2)
-            if random.randint(0, 1) == 1:
-                opt += "--tp_aware "
-            p_list.append(self.urma_perftest_one_perf_single_dev(host1, host2, opt=opt))
+        # Traverse all hosts, all modes, and all types and cover both aware TP to generate pairwise traffic
+        for host1, host2 in combinations(self.host_list, 2):
+            for cmd in cmd_list:
+                for mode in mode_list:
+                    if "-p 2" in mode and "send_" not in cmd:
+                        continue
+                    p_list.append(self.urma_perftest_one_perf_single_dev(host1, host2, cmd_syntax=cmd, opt=mode))
