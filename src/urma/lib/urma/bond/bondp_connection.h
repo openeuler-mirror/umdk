5b4eba3d10088b2afd07b7874e2a2b0967fa56d2
wanghang73 <wanghang73@huawei.com>
2026-04-29 20:59:10 +0800

    urma: bonding device remove target jetty from vconn

--- src/urma/lib/urma/bond/bondp_connection.h
+++ src/urma/lib/urma/bond/bondp_connection.h
@@ -21,15 +21,10 @@
  * Virtual connection is used to implement de-duplication between two vjettys.
  */
 typedef struct bondp_v_connection {
-    /* only valid on sender side */
-    bondp_target_jetty_t *target_vjetty;
     /* de-duplication */
-    uint32_t msn;
     bdp_slide_wnd_t recv_wnd;
     bool is_init;
 } bdp_v_conn_t;
-
-void init_v_conn_on_send(bdp_v_conn_t *v_conn, void *target_vjetty, int target_dev_num);
 
 typedef struct bdp_v_conn_node {
     hmap_node_t hmap_node;
@@ -40,9 +35,6 @@ typedef struct bdp_v_conn_node {
 int bdp_v_conn_table_create(bondp_hash_table_t *tbl, uint32_t size);
 
 bdp_v_conn_t *bdp_v_conn_table_lookup(bondp_hash_table_t *tbl, urma_jetty_id_t *target_id);
-
-int bdp_v_conn_table_add_on_send(bondp_hash_table_t *tbl, urma_jetty_id_t *target_id,
-                                 void *target_vjetty, int target_dev_num, bdp_v_conn_t **v_conn_out, bool is_standalone);
 
 int bdp_v_conn_table_add_on_recv(bondp_hash_table_t *tbl, urma_jetty_id_t *target_id,
                                  bdp_v_conn_t **v_conn_out, bool is_standalone);
