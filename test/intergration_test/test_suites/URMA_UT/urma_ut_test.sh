2dcd0ad84ea70f1dbaf955900c219c99cf826d97
Wen Chen <chenwen54@huawei.com>
2026-05-07 12:09:29 +0800

    urma: support ut use cases
    (cherry picked from commit 6a5814f5ce7c77051cc443e3959dd1a7e7d0e048)

--- test/intergration_test/test_suites/URMA_UT/urma_ut_test.sh
+++ test/intergration_test/test_suites/URMA_UT/urma_ut_test.sh
@@ -14,20 +14,6 @@ TARGET_YAML_PATH="$TARGET_YAML_DIR/test_env.yaml"
 
 LOG_SWITCH=${ENABLE_LOG:-"false"}
 
-function run_test() {
-    local test_file=$1
-
-    if [ "$LOG_SWITCH" = "true" ]; then
-        export PYTEST_ADDOPTS="--log-cli-level=INFO -s"
-    else
-        export PYTEST_ADDOPTS="--log-cli-level=ERROR"
-    fi
-
-    echo "------------------------------------------------"
-    echo "Running pytest for: $test_file (Log Switch: $LOG_SWITCH)"
-    pytest "$test_file"
-}
-
 function prepare_and_run() {
     echo $SRC_BUILD_DIR
     mkdir -p "$SRC_BUILD_DIR"
@@ -36,26 +22,26 @@ function prepare_and_run() {
     if [ -d "./build" ]; then
         rm -rf build
     fi
-    #rpm -ivh /root/rpmbuild/RPMS/aarch64/umdk-urma-kmod-*.rpm
+
     cmake .. -D BUILD_ALL=disable -D BUILD_URMA=enable
     make install -j
     echo "URMA package installed successfully."
     
-    #systemctl stop ubse
-    #sleep 1
-    #rmmod ubagg || exit 1
-    #rmmod uburma || exit 1
-    #rmmod ipourma || exit 1
-    #rmmod udma
-    #rmmod ubcore
-    #modprobe ubcore g_ubcore_log_level=6
-    #modprobe uburma g_uburma_log_level=6
-    #modprobe ubagg
-    #modprobe ipourma tx_ring_size=16 rx_ring_size-32 page_level=16 ctp_sl=6
-    #modprobe udma debug_switch=1 jfc_arm_mode=2 well_know_jetty_pgsz_check=0
-    #systemctl start ubse
-    #sleep 1
-    #echo "URMA drivers update installed successfully."
+    systemctl stop ubse
+    sleep 1
+    rmmod ubagg || exit 1
+    rmmod uburma || exit 1
+    rmmod ipourma || exit 1
+    rmmod udma || exit 1
+    rmmod ubcore || exit 1
+    modprobe ubcore
+    modprobe uburma
+    modprobe ubagg
+    modprobe ipourma tx_ring_size=16 rx_ring_size=32 page_level=16 ctp_sl=6
+    modprobe udma debug_switch=1 jfc_arm_mode=2 well_known_jetty_pgsz_check=0
+    systemctl start ubse
+    sleep 1
+    echo "URMA drivers update installed successfully."
     echo "Configuring test environment YAML..."
     if [ ! -d "$TARGET_YAML_DIR" ]; then
         mkdir -p "$TARGET_YAML_DIR"
@@ -69,13 +55,8 @@ function prepare_and_run() {
 
     echo "Starting Pytest execution..."
     cd "$CURRENT_PATH"
-
-    run_test "./test_urma_basic_loopback/test.py"
-    run_test "./test_bonding_brpc/test.py"
-    run_test "./test_bonding_hcom/test.py"
-    run_test "./test_bonding_kvache/test.py"
-    run_test "./test_bonding_vm_migration/test.py"
-
+    find ./ -name test.py | xargs -i pytest {}
+    find ../URMA -name test.py | xargs -i pytest {}
     echo "========================================"
     echo "All tasks finished at $(date)"
     exit 0
