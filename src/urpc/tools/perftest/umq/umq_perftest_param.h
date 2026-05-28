338b678b3a97d37c7628cc1499cda5fede61acec
w00669608 <wangxin554@huawei.com>
2026-05-28 10:03:38 +0800

    umq: add 4K limit for ctp

--- src/urpc/tools/perftest/umq/umq_perftest_param.h
+++ src/urpc/tools/perftest/umq/umq_perftest_param.h
@@ -31,6 +31,7 @@ typedef struct umq_perftest_config {
     uint32_t test_round;
     umq_buf_mode_t buf_mode;
     umq_tp_mode_t tp_mode;
+    umq_tp_type_t tp_type;
     uint16_t cna;
     uint32_t deid;
     uint16_t eid_idx;
