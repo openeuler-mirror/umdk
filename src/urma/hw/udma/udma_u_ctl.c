openeuler-mirror/umdk

commit a6066184178573c5b7f5dc1d5dd4c27e335b93a2

ub: udma: resolve issues of create ccu_jetty
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/umdk/issues/494
CVE: NA

-----------------------------------------------------------

This patch resolves issues
related to the creation of ccu_jetty.

Fixes: 260ceef1798e ("ub: udma: Support user ctl function.")

Signed-off-by: Jiawei Kang <kangjiawei2@huawei.com>

src/urma/hw/udma/udma_u_ctl.c
@@ -605,6 +605,8 @@ static int udma_u_jfc_cmd_ex(urma_context_t *ctx, struct udma_u_jfc *jfc,
 
 	cmd.mode = (uint32_t)jfc->mode;
 	cmd.buf_len = jfc_depth;
+	if (jfc->mode == UDMA_U_LOCK_CCU_JFC_TYPE)
+		cmd.ccu_cqe_flag = !!jfc->ccu_flag;
 
 	udma_u_set_udata(&udata, &cmd, sizeof(cmd), NULL, 0);
 
@@ -633,7 +635,7 @@ static int udma_u_verify_jfc_param_ex(urma_context_t *ctx, urma_jfc_cfg_t *base_
 }
 
 static urma_jfc_t *udma_u_create_base_jfc_ex(urma_context_t *ctx, urma_jfc_cfg_t *base_cfg,
-					     enum udma_u_jfc_type jfc_mode)
+					     enum udma_u_jfc_type jfc_mode, uint32_t ccu_cqe_flag)
 {
 	struct udma_u_jfc *jfc;
 	uint32_t depth;
@@ -658,6 +660,9 @@ static urma_jfc_t *udma_u_create_base_jfc_ex(urma_context_t *ctx, urma_jfc_cfg_t
 
 	depth = base_cfg->depth < UDMA_U_MIN_JFC_DEPTH ?
 		UDMA_U_MIN_JFC_DEPTH : roundup_pow_of_two(base_cfg->depth);
+
+	if (jfc->mode == UDMA_U_LOCK_CCU_JFC_TYPE)
+		jfc->ccu_flag = ccu_cqe_flag;
 
 	ret = udma_u_jfc_cmd_ex(ctx, jfc, base_cfg, depth);
 	if (ret) {
@@ -688,7 +693,7 @@ static urma_jfc_t *udma_u_create_jfc_ex(urma_context_t *ctx,
 		return NULL;
 	}
 
-	return udma_u_create_base_jfc_ex(ctx, &cfg_ex->base_cfg, cfg_ex->jfc_mode);
+	return udma_u_create_base_jfc_ex(ctx, &cfg_ex->base_cfg, cfg_ex->jfc_mode, NULL);
 }
 
 static int udma_u_jfr_ops_ex(urma_context_t *ctx, urma_user_ctl_in_t *in,
@@ -832,6 +837,168 @@ static int udma_u_jetty_ops_ex(urma_context_t *ctx, urma_user_ctl_in_t *in,
 
 		(void)memcpy(&cfg_ex, (void *)in->addr, sizeof(struct udma_u_jetty_cfg_ex));
 		jetty = udma_u_create_jetty_ex(ctx, &cfg_ex);
+		if (jetty == NULL)
+			return EFAULT;
+
+		udma_fill_create_jetty_ex_out(out, jetty);
+		atomic_fetch_add(&ctx->ref.atomic_cnt, 1);
+	} else {
+		if (!udma_u_user_ctl_check_param(in->addr, in->len, (uint32_t)sizeof(urma_jetty_t *), op))
+			return EINVAL;
+
+		(void)memcpy(&jetty, (void *)in->addr, sizeof(urma_jetty_t *));
+		if (jetty == NULL)
+			return EINVAL;
+
+		if (udma_u_delete_jetty(jetty))
+			return EFAULT;
+
+		atomic_fetch_sub(&ctx->ref.atomic_cnt, 1);
+	}
+
+	return 0;
+}
+
+static int udma_u_verify_lock_buffer_jetty_param(urma_context_t *ctx,
+						 struct udma_u_lock_jetty_cfg *lock_cfg)
+{
+#define WQE_LOCK_BUFFER_JETTY_SIZE 4096
+#define WQE_LOCK_BUFFER_JETTY_DEPTH_SHIFT 5
+	struct udma_u_context *udma_ctx = to_udma_u_ctx(ctx);
+
+	if (!udma_ctx->lock_buffer_en) {
+		UDMA_LOG_ERR("no support create lock buffer JETTY.\n");
+		return EINVAL;
+	}
+
+	if (lock_cfg->base_cfg.jfs_cfg.priority >= UDMA_MAX_PRIORITY) {
+		UDMA_LOG_ERR("user mode JETTY priority is out of range, priority is %u.\n",
+			     lock_cfg->base_cfg.jfs_cfg.priority);
+		return EINVAL;
+	}
+
+	if (udma_u_check_jetty_param_ex(ctx, &lock_cfg->base_cfg))
+		return EINVAL;
+
+	if (lock_cfg->buf_idx >= WQE_LOCK_BUFFER_JETTY_SIZE) {
+		UDMA_LOG_ERR("buffer index is out of range.\n");
+		return EINVAL;
+	}
+
+	if (udma_ctx->lock_buf_bb_shift > WQE_LOCK_BUFFER_JETTY_DEPTH_SHIFT) {
+		UDMA_LOG_ERR("lock buffer bit block shift is out of range.\n");
+		return EINVAL;
+	}
+
+	return 0;
+}
+
+static int udma_u_init_lock_jetty_queue_buf(struct udma_u_jetty_queue *q,
+					    struct udma_u_lock_jetty_cfg *lock_cfg, uint32_t jetty_depth)
+{
+	uint32_t buf_shift;
+
+	q->baseblk_shift = UDMA_JFS_WQEBB_SHIFT;
+	buf_shift = align_power2(jetty_depth * q->sqe_bb_cnt * UDMA_JFS_WQEBB);
+	q->qbuf_size = (1U << buf_shift);
+	q->baseblk_cnt = q->qbuf_size >> q->baseblk_shift;
+	q->baseblk_mask = q->baseblk_cnt - 1U;
+
+	q->wrid = (uintptr_t *)calloc(q->baseblk_cnt, sizeof(uint64_t));
+	if (q->wrid == NULL) {
+		UDMA_LOG_ERR("failed to alloc lock buffer for work request id!\n");
+		return ENOMEM;
+	}
+
+	q->qbuf = (void *)(uintptr_t)lock_cfg->buf_idx;
+
+	return 0;
+}
+
+static int udma_u_create_lock_sq(struct udma_u_jetty_queue *sq,
+			         struct udma_u_lock_jetty_cfg *lock_cfg, uint32_t jetty_depth)
+{
+#define LOCK_SQE_BB_CNT 2
+	urma_jetty_cfg_t *base_cfg = &lock_cfg->base_cfg;
+	int ret;
+
+	sq->lock_free = !!base_cfg->jfs_cfg.flag.bs.lock_free;
+	if (!sq->lock_free &&
+	    pthread_spin_init(&sq->lock, PTHREAD_PROCESS_PRIVATE))
+		return EINVAL;
+
+	sq->max_inline_size = base_cfg->jfs_cfg.max_inline_data;
+	sq->max_sge_num = base_cfg->jfs_cfg.max_sge;
+	sq->sqe_bb_cnt = LOCK_SQE_BB_CNT;
+	ret = udma_u_init_lock_jetty_queue_buf(sq, lock_cfg, jetty_depth);
+	if (ret) {
+		UDMA_LOG_ERR("init queue buffer wrong, ret = %d.\n", ret);
+		goto err_init_sq_buf;
+	}
+
+	return 0;
+
+err_init_sq_buf:
+	if (!sq->lock_free)
+		(void)pthread_spin_destroy(&sq->lock);
+	return EFAULT;
+}
+
+urma_jetty_t *udma_u_create_lock_buffer_jetty(urma_context_t *ctx, struct udma_u_lock_jetty_cfg *lock_cfg)
+{
+	struct udma_u_context *udma_ctx = to_udma_u_ctx(ctx);
+	urma_jetty_cfg_t *cfg = &lock_cfg->base_cfg;
+	struct udma_u_jetty *jetty;
+	uint32_t jetty_depth = 0;
+	int ret;
+
+	if (udma_u_verify_lock_buffer_jetty_param(ctx, lock_cfg))
+		return NULL;
+
+	jetty = (struct udma_u_jetty *)calloc(1, sizeof(struct udma_u_jetty));
+	if (jetty == NULL) {
+		UDMA_LOG_ERR("memory allocation failed.\n");
+		return NULL;
+	}
+
+	jetty->sq.sq_reserved = udma_ctx->sq_reserved;
+	if (init_jetty_trans_mode(jetty, cfg))
+		goto err_init_jetty_trans_mode;
+	jetty_depth = (1 << udma_ctx->lock_buf_bb_shift);
+	ret = udma_u_create_lock_sq(&jetty->sq, lock_cfg, jetty_depth);
+	if (ret) {
+		UDMA_LOG_ERR("jetty create lock sq failed, ret = %d.\n", ret);
+		goto err_init_jetty_trans_mode;
+	}
+
+	jetty->jfr = to_udma_u_jfr(cfg->shared.jfr);
+	jetty->jetty_type = UDMA_LOCK_BUFFER_JETTY_TYPE;
+	jetty->sq.cstm = 1;
+	if (udma_u_create_common_jetty(ctx, jetty, cfg))
+		goto err_jetty_create_common_jetty;
+
+	return &jetty->base;
+err_jetty_create_common_jetty:
+	udma_u_lock_delete_sq(&jetty->sq);
+err_init_jetty_trans_mode:
+	free(jetty);
+
+	return NULL;
+}
+
+static int udma_u_jetty_lock_buffer_ops(urma_context_t *ctx, urma_user_ctl_in_t *in,
+					urma_user_ctl_out_t *out, enum udma_u_user_ctl_opcode op)
+{
+	struct udma_u_lock_jetty_cfg cfg_ex;
+	urma_jetty_t *jetty = NULL;
+
+	if (op == UDMA_U_USER_CTL_CREATE_LOCK_BUFFER_JETTY_EX) {
+		if (!udma_u_user_ctl_check_param(in->addr, in->len, (uint32_t)sizeof(struct udma_u_lock_jetty_cfg), op) ||
+		    !udma_u_user_ctl_check_param(out->addr, out->len, (uint32_t)sizeof(struct udma_u_jetty_info), op))
+			return EINVAL;
+
+		(void)memcpy(&cfg_ex, (void *)in->addr, sizeof(struct udma_u_lock_jetty_cfg));
+		jetty = udma_u_create_lock_buffer_jetty(ctx, &cfg_ex);
 		if (jetty == NULL)
 			return EFAULT;
 
@@ -1146,6 +1313,54 @@ static int udma_u_query_ae_aux_info(urma_context_t *ctx, urma_user_ctl_in_t *in,
 	return ret;
 }
 
+urma_jfc_t *udma_u_create_ccu_jfc(urma_context_t *ctx, struct udma_u_lock_jfc_cfg *cfg_ex)
+{
+	struct udma_u_context *udma_ctx = to_udma_u_ctx(ctx);
+
+	if (!udma_ctx->ccu_jfc_property_en) {
+		UDMA_LOG_ERR("no support create CCU JFC.\n");
+		return NULL;
+	}
+
+	return udma_u_create_base_jfc_ex(ctx, &cfg_ex->base_cfg, UDMA_U_LOCK_CCU_JFC_TYPE,
+					 cfg_ex->ccu_cfg.ccu_cqe_flag);
+}
+
+static int udma_u_ccu_jfc_ops(urma_context_t *ctx, urma_user_ctl_in_t *in,
+			      urma_user_ctl_out_t *out, enum udma_u_user_ctl_opcode op)
+{
+	struct udma_u_lock_jfc_cfg cfg_ex;
+	urma_jfc_t *jfc = NULL;
+
+	if (op == UDMA_U_USER_CTL_CREATE_CCU_JFC_EX) {
+		if (!udma_u_user_ctl_check_param(in->addr, in->len, (uint32_t)sizeof(struct udma_u_lock_jfc_cfg), op) ||
+		    !udma_u_user_ctl_check_param(out->addr, out->len, (uint32_t)sizeof(urma_jfc_t *), op))
+			return EINVAL;
+
+		(void)memcpy(&cfg_ex, (void *)(uintptr_t)in->addr, sizeof(struct udma_u_lock_jfc_cfg));
+		jfc = udma_u_create_ccu_jfc(ctx, &cfg_ex);
+		if (jfc == NULL)
+			return EFAULT;
+
+		memcpy((void *)out->addr, &jfc, sizeof(urma_jfc_t *));
+		atomic_fetch_add(&ctx->ref.atomic_cnt, 1);
+	} else {
+		if (!udma_u_user_ctl_check_param(in->addr, in->len, (uint32_t)sizeof(urma_jfc_t *), op))
+			return EINVAL;
+
+		(void)memcpy(&jfc, (void *)(uintptr_t)in->addr, sizeof(urma_jfc_t *));
+		if (jfc == NULL)
+			return EFAULT;
+
+		if (udma_u_delete_jfc(jfc))
+			return EFAULT;
+
+		atomic_fetch_sub(&ctx->ref.atomic_cnt, 1);
+	}
+
+	return 0;
+}
+
 static udma_u_user_ctl_ops g_udma_u_user_ctl_ops[] = {
 	[UDMA_U_USER_CTL_CREATE_JFR_EX] = udma_u_jfr_ops_ex,
 	[UDMA_U_USER_CTL_DELETE_JFR_EX] = udma_u_jfr_ops_ex,
@@ -1161,6 +1376,10 @@ static udma_u_user_ctl_ops g_udma_u_user_ctl_ops[] = {
 	[UDMA_U_USER_CTL_QUERY_TP_SPORT] = udma_u_ctrlq_query_tp_sport,
 	[UDMA_U_USER_CTL_QUERY_CQE_AUX_INFO] = udma_u_query_cqe_aux_info,
 	[UDMA_U_USER_CTL_QUERY_AE_AUX_INFO] = udma_u_query_ae_aux_info,
+	[UDMA_U_USER_CTL_CREATE_LOCK_BUFFER_JETTY_EX] = udma_u_jetty_lock_buffer_ops,
+	[UDMA_U_USER_CTL_DELETE_LOCK_BUFFER_JETTY_EX] = udma_u_jetty_lock_buffer_ops,
+	[UDMA_U_USER_CTL_CREATE_CCU_JFC_EX] = udma_u_ccu_jfc_ops,
+	[UDMA_U_USER_CTL_DELETE_CCU_JFC_EX] = udma_u_ccu_jfc_ops,
 };
 
 bool udma_u_user_ctl_check_param(uint64_t addr, uint32_t in_len, uint32_t len,

src/urma/hw/udma/include/udma_u_ctl.h
@@ -91,11 +91,28 @@ enum udma_u_jfc_type {
 	UDMA_U_NORMAL_JFC_TYPE,
 	UDMA_U_STARS_JFC_TYPE,
 	UDMA_U_CCU_JFC_TYPE,
+	UDMA_RVS_TYPE,
+	UDMA_U_LOCK_CCU_JFC_TYPE,
 };
 
 struct udma_u_jfc_cfg_ex {
 	urma_jfc_cfg_t base_cfg;
 	enum udma_u_jfc_type jfc_mode;
+};
+
+struct udma_u_lock_jetty_cfg {
+	urma_jetty_cfg_t base_cfg;
+	bool jetty_type; /* 1:wqe lock buffer jetty, 0:normal jetty, defualt: 1 */
+	uint32_t buf_idx; /* buf offset addres idx */
+};
+
+struct udma_u_jfc_ccu_cfg {
+    uint32_t ccu_cqe_flag;
+};
+
+struct udma_u_lock_jfc_cfg {
+	urma_jfc_cfg_t base_cfg;
+	struct udma_u_jfc_ccu_cfg ccu_cfg;
 };
 
 struct udma_u_jfs_wr_ex {
@@ -166,6 +183,10 @@ enum udma_u_user_ctl_opcode {
 	UDMA_U_USER_CTL_QUERY_TP_SPORT,
 	UDMA_U_USER_CTL_QUERY_CQE_AUX_INFO,
 	UDMA_U_USER_CTL_QUERY_AE_AUX_INFO,
+	UDMA_U_USER_CTL_CREATE_LOCK_BUFFER_JETTY_EX,
+	UDMA_U_USER_CTL_DELETE_LOCK_BUFFER_JETTY_EX,
+	UDMA_U_USER_CTL_CREATE_CCU_JFC_EX,
+	UDMA_U_USER_CTL_DELETE_CCU_JFC_EX,
 	UDMA_U_USER_CTL_MAX,
 };
 

src/urma/hw/udma/kernel_headers/udma_abi.h
@@ -10,7 +10,7 @@
 #define MAP_INDEX_MASK 0xfffffff
 #define MAP_INDEX_SHIFT 4
 
-#define UDMA_SEGMENT_ACCESS_GUARD (1UL << 5)
+#define UDMA_SEGMENT_ACCESS_GUARD 0xf
 
 #define UDMA_CQE_COALESCE_SHIFT 10
 #define UDMA_CQE_COALESCE_CNT_MAX (1 << UDMA_CQE_COALESCE_SHIFT)
@@ -48,6 +48,7 @@ enum udma_jetty_type {
 	UDMA_CCU_JETTY_TYPE,
 	UDMA_NORMAL_JETTY_TYPE,
 	UDMA_URMA_NORMAL_JETTY_TYPE,
+	UDMA_LOCK_BUFFER_JETTY_TYPE,
 	UDMA_URMA_EX_JETTY_TYPE,
 	UDMA_JETTY_TYPE_MAX
 };
@@ -97,7 +98,8 @@ struct udma_create_jfc_ucmd {
 	__aligned_u64 db_addr;
 	__u32 is_hugepage : 1;
 	__u32 dtu_en : 1;
-	__u32 rsv : 30;
+	__u32 ccu_cqe_flag : 1;
+	__u32 rsv : 29;
 	__u32 rsv1;
 };
 
@@ -112,7 +114,10 @@ struct udma_create_ctx_resp {
 	__u32 sva_sep_mode_en : 1;
 	__u32 u_dtu_enable : 1;
 	__u32 st64b_en : 1;
-	__u32 rsv : 15;
+	__u32 lock_buffer_en : 1;
+	__u32 ccu_jfc_property_en : 1;
+	__u32 lock_buf_bb_shift : 8;
+	__u32 rsv : 5;
 	__u32 ue_id;
 	__u32 chip_id;
 	__u32 die_id;

src/urma/hw/udma/udma_u_common.h
@@ -24,6 +24,7 @@
 #include "udma_u_abi.h"
 #include "udma_u_log.h"
 
+#define UDMA_WQE_LOCK_BUFFER_JETTY_IDX 4096
 #define UDMA_JFS_WQEBB 64
 #define UDMA_JFR_WQEBB 16U
 #define UDMA_MIN_JFS_WQEBB_CNT 64
@@ -35,6 +36,8 @@
 #define UDMA_BITS_PER_LONG_SHIFT 6
 #define UDMA_JFS_WQEBB_SHIFT 6
 #define UDMA_SGE_SIZE 16
+#define UDMA_CCU_SQE_BB_CNT 2
+#define SQE_VA_L_OFFSET 12U
 
 #define UDMA_JFC_DB_OFFSET 0
 #define UDMA_MIN(x, y) ((x) < (y) ? (x) : (y))
@@ -101,6 +104,9 @@ struct udma_u_context {
 	uint32_t		jfr_sge;
 	bool			sq_reserved;
 	bool			st64b_en;
+	bool			lock_buffer_en;
+	bool			ccu_jfc_property_en;
+	uint8_t 		lock_buf_bb_shift;
 	bool			dtu_enable;
 	uint64_t		dtu_va_base;
 	uint64_t		dtu_va_size;
@@ -162,6 +168,8 @@ struct udma_u_jetty_queue {
 	bool pi_type;
 	bool dtu_en;
 	uint32_t aligned_size;
+	bool is_jetty;
+	bool is_wqe_lock_buf_jetty;
 };
 
 struct udma_wqe_sge {
@@ -215,6 +223,7 @@ struct udma_u_jfc {
 	uint32_t arm_sn;
 	uint32_t mode;
 	uint32_t cq_shift;
+	uint32_t ccu_flag;
 };
 
 struct udma_u_tid {

src/urma/hw/udma/udma_u_jetty.c
@@ -212,6 +212,7 @@ static int udma_u_active_jetty_prepare(urma_jetty_t *jetty, urma_jetty_cfg_t *cf
 
 	udma_jetty->sq.sq_reserved = udma_jetty->sq.ctx->sq_reserved;
 	udma_jetty->sq.db.id = jetty->jetty_cfg.id;
+	udma_jetty->sq.is_jetty = true;
 	ret = udma_u_create_sq(&udma_jetty->sq, &cfg->jfs_cfg);
 	if (ret) {
 		UDMA_LOG_ERR("active JETTY create SQ failed, ret = %d.\n", ret);
@@ -347,7 +348,10 @@ static void udma_u_free_jetty_prepare(urma_jetty_t* jetty)
 
 	udma_u_jetty_table_remove(udma_ctx, udma_jetty);
 	udma_u_free_db(jetty->urma_ctx, &udma_jetty->sq.db);
-	udma_u_delete_sq(&udma_jetty->sq);
+	if (udma_jetty->jetty_type == UDMA_LOCK_BUFFER_JETTY_TYPE)
+		udma_u_lock_delete_sq(&udma_jetty->sq);
+	else
+		udma_u_delete_sq(&udma_jetty->sq);
 	remove_jetty_from_grp(udma_jetty);
 }
 
@@ -901,8 +905,8 @@ urma_status_t udma_u_set_jetty_field(struct udma_u_jetty_queue *sq, uint64_t opt
 	switch (opt) {
 	case URMA_JFS_SQE_BASE_ADDR:
 		sq->qbuf = (void *)(uintptr_t)(*((uint64_t *)buf));
-		if (!sq->qbuf) {
-			UDMA_LOG_ERR("set SQE base address is null.\n");
+		if (!sq->qbuf && !sq->ctx->lock_buffer_en) {
+			UDMA_LOG_ERR("set SQE base address is null and no support wqe lock buffer.\n");
 			return URMA_EINVAL;
 		}
 		sq->cstm = true;
@@ -1103,6 +1107,32 @@ static int udma_u_active_jetty_after(urma_jetty_t *jetty)
 	return ret;
 }
 
+static int udma_u_check_wqe_lock_buffer_jetty(struct udma_u_jetty *udma_jetty)
+{
+	if (!udma_jetty->sq.cstm) {
+		UDMA_LOG_ERR("ccu jetty no support address not configured.\n");
+		return URMA_EINVAL;
+	}
+
+	if (((uint64_t)udma_jetty->sq.qbuf) >= UDMA_WQE_LOCK_BUFFER_JETTY_IDX) {
+		UDMA_LOG_ERR("buffer index is out of range, buffer index = %lu.\n",
+			     (uint64_t)udma_jetty->sq.qbuf);
+		return URMA_EINVAL;
+	}
+
+	return 0;
+}
+
+static int udma_u_check_normal_jetty(struct udma_u_jetty *udma_jetty)
+{
+	if (udma_jetty->sq.cstm && !udma_jetty->sq.qbuf) {
+		UDMA_LOG_ERR("set SQE base address is null.\n");
+		return URMA_EINVAL;
+	}
+
+	return 0;
+}
+
 urma_status_t udma_u_active_jetty(urma_jetty_t *jetty)
 {
 	struct udma_u_context *udma_ctx = to_udma_u_ctx(jetty->urma_ctx);
@@ -1110,6 +1140,16 @@ urma_status_t udma_u_active_jetty(urma_jetty_t *jetty)
 	urma_jetty_cfg_t *cfg = &jetty->jetty_cfg;
 	urma_cmd_udrv_priv_t udata = {};
 	int ret;
+
+    udma_jetty->sq.is_wqe_lock_buf_jetty =
+		is_support_wqe_lock_buffer_jetty(udma_jetty->sq.ctx, jetty->jetty_cfg.id, true);
+	if (udma_jetty->sq.is_wqe_lock_buf_jetty) {
+		if (udma_u_check_wqe_lock_buffer_jetty(udma_jetty))
+			return URMA_EINVAL;
+	} else {
+		if (udma_u_check_normal_jetty(udma_jetty))
+			return URMA_EINVAL;
+	}
 
 	udma_jetty->sq.dtu_en = udma_ctx->dtu_enable;
 	ret = udma_u_active_jetty_prepare(jetty, cfg);

src/urma/hw/udma/udma_u_jfs.c
@@ -190,6 +190,7 @@ int udma_u_exec_jfs_create_cmd(urma_context_t *ctx, struct udma_u_jfs *jfs,
 int udma_u_create_sq(struct udma_u_jetty_queue *sq, urma_jfs_cfg_t *cfg)
 {
 	struct udma_u_context *ctx = sq->ctx;
+	uint32_t jetty_depth = cfg->depth;
 	uint32_t aligned_size;
 	uint32_t sqe_bb_cnt;
 	bool is_ccu_jetty;
@@ -208,6 +209,13 @@ int udma_u_create_sq(struct udma_u_jetty_queue *sq, urma_jfs_cfg_t *cfg)
 	if (sqe_bb_cnt > MAX_SQE_BB_NUM)
 		sqe_bb_cnt = MAX_SQE_BB_NUM;
 	sq->sqe_bb_cnt = sqe_bb_cnt;
+	if (sq->is_wqe_lock_buf_jetty) {
+		sq->sqe_bb_cnt = UDMA_CCU_SQE_BB_CNT;
+		jetty_depth = (1 << ctx->lock_buf_bb_shift);
+		sq->qbuf = (void *)(((uint64_t)sq->qbuf) << SQE_VA_L_OFFSET);
+		sq->qbuf_size = sq->sqe_bb_cnt * jetty_depth * UDMA_JFS_WQEBB;
+		return 0;
+	}
 	sq->max_sge_num = cfg->max_sge;
 
 	is_ccu_jetty = ctx->ccu_jetty_max_cnt &&
@@ -216,7 +224,7 @@ int udma_u_create_sq(struct udma_u_jetty_queue *sq, urma_jfs_cfg_t *cfg)
 	aligned_size = is_ccu_jetty ?
 		(UDMA_MIN_CCU_WQEBB_CNT * UDMA_JFS_WQEBB) : UDMA_HW_PAGE_SIZE;
 	sq->aligned_size = aligned_size;
-	if (!udma_u_alloc_queue_buf(sq, sqe_bb_cnt * cfg->depth,
+	if (!udma_u_alloc_queue_buf(sq, sq->sqe_bb_cnt * jetty_depth,
 				    UDMA_JFS_WQEBB, aligned_size, true)) {
 		UDMA_LOG_ERR("failed to alloc JFS work queue entry buffer.\n");
 		goto err_alloc_buf;

src/urma/hw/udma/udma_u_jfs.h
@@ -177,6 +177,17 @@ static inline uint32_t get_max_sge_num(uint8_t max_sge, uint32_t max_inline_size
 	return UDMA_MAX(max_sge, size);
 }
 
+static inline bool is_support_wqe_lock_buffer_jetty(struct udma_u_context *udma_ctx,
+						    uint32_t id, bool is_jetty)
+{
+	bool is_ccu_jetty;
+
+	is_ccu_jetty = udma_ctx->ccu_jetty_max_cnt &&
+		       id >= udma_ctx->ccu_jetty_start_id &&
+		       id < (udma_ctx->ccu_jetty_start_id + udma_ctx->ccu_jetty_max_cnt);
+	return is_jetty && is_ccu_jetty && udma_ctx->lock_buffer_en;
+}
+
 urma_status_t udma_u_post_sq_wr(struct udma_u_context *udma_ctx,
 				struct udma_u_jetty_queue *sq, urma_jfs_wr_t *wr,
 				urma_jfs_wr_t **bad_wr);

src/urma/hw/udma/udma_u_ops.c
@@ -150,6 +150,9 @@ static void udma_u_init_context(struct udma_u_context *udma_ctx,
 	udma_ctx->dump_aux_info = resp->dump_aux_info;
 	udma_ctx->jfr_sge = resp->jfr_sge;
 	udma_ctx->sq_reserved = resp->sq_reserved;
+	udma_ctx->lock_buffer_en = resp->lock_buffer_en;
+	udma_ctx->ccu_jfc_property_en = resp->ccu_jfc_property_en;
+	udma_ctx->lock_buf_bb_shift = resp->lock_buf_bb_shift;
 	udma_ctx->dtu_va_base = resp->dtu_va_base;
 	udma_ctx->dtu_va_size = resp->dtu_va_size;
 	udma_ctx->dtu_enable = resp->u_dtu_enable;

commit a4d598edbb9340bfaa672bf07d65914c6c06962c

ub: udma: fix Bazel compile failures
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/umdk/issues/597
CVE: NA

-----------------------------------------------------------

Fix UDMA Bazel build failures when warnings are treated as errors.

Pass an integer zero to udma_u_create_base_jfc_ex() for the normal
JFC creation path because the CCU CQE flag argument is a uint32_t
and is only used by the CCU JFC mode.

Keep g_udma_provider_ops const-qualified and make the UDMA Bazel
target report discarded-qualifiers as a warning instead of an error.

Fixes: a60661841785 ("ub: udma: resolve issues of create ccu_jetty")
Signed-off-by: luyizhou <luyizhou1@huawei.com>

src/urma/BUILD.bazel
@@ -92,6 +92,7 @@ URMA_TOOL_LINKOPTS = [
 URMA_UDMA_COPTS = URMA_COMMON_COPTS + [
     "-fno-common",
     "-Werror=implicit-function-declaration",
+    "-Wno-error=discarded-qualifiers",
     "-ftrapv",
 ] + select({
     ":udma_st64b_disabled": [],

src/urma/hw/udma/udma_u_ctl.c
@@ -693,7 +693,7 @@ static urma_jfc_t *udma_u_create_jfc_ex(urma_context_t *ctx,
 		return NULL;
 	}
 
-	return udma_u_create_base_jfc_ex(ctx, &cfg_ex->base_cfg, cfg_ex->jfc_mode, NULL);
+	return udma_u_create_base_jfc_ex(ctx, &cfg_ex->base_cfg, cfg_ex->jfc_mode, 0);
 }
 
 static int udma_u_jfr_ops_ex(urma_context_t *ctx, urma_user_ctl_in_t *in,