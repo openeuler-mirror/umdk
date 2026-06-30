df1f2cce015c36a277c85678511f0f0cf1d6598a
yczhaocau <zhaoyanchao3@huawei.com>
2026-06-30 16:22:55 +0800

    add op_api of fuesd_deep_moe

---
+++ src/cam/comm_operator/ascend_kernels/fused_deep_moe/op_api/aclnn_fused_deep_moe.cpp
@@ -0,0 +1,84 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Description: add fused deep moe interface cpp file.
+ * Create: 2025-07-21
+ * Note:
+ * History: 2025-07-21 add fused deep moe interface cpp file.
+ */
+
+#include <string.h>
+#include "graph/types.h"
+#include "aclnn/opdev/platform.h"
+#include "aclnnInner_fused_deep_moe.h"
+#include "aclnn_fused_deep_moe.h"
+
+namespace {
+static constexpr int32_t NNOPBASE_HCCL_SERVER_TYPE_AICPU = 0;
+static constexpr int32_t NNOPBASE_HCCL_SERVER_TYPE_MTE = 1;
+static constexpr int32_t NNOPBASE_HCCL_SERVER_TYPE_END = 2;
+} // namespace
+extern "C" void __attribute__((weak)) NnopbaseSetHcclServerType(void *executor, int32_t sType);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+aclnnStatus aclnnFusedDeepMoeGetWorkspaceSize(
+    const aclTensor *x,
+    const aclTensor *expertIds,
+    const aclTensorList *gmm1Weight,
+    const aclTensorList *gmm1WeightScale,
+    const aclTensorList *gmm2Weight,
+    const aclTensorList *gmm2WeightScale,
+    const aclTensor *expertScales,
+    const aclTensor *shareGmm1WeightOptional,
+    const aclTensor *shareGmm1WeightScaleOptional,
+    const aclTensor *shareGmm2WeightOptional,
+    const aclTensor *shareGmm2WeightScaleOptional,
+    const aclTensor *expertSmoothScalesOptional,
+    const aclTensor *shareSmoothScalesOptional,
+    const aclTensor *xActiveMaskOptional,
+    char *groupEp,
+    int64_t epRankSize,
+    int64_t epRankId,
+    int64_t moeExpertNum,
+    int64_t quantMode,
+    int64_t globalBs,
+    int64_t extInfo,
+    int64_t shmemWorkspace,
+    const aclTensor *output,
+    const aclTensor *shareOutput,
+    const aclTensor *expertTokenNums,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor)
+{
+    return aclnnInnerFusedDeepMoeGetWorkspaceSize(x, expertIds, gmm1Weight, gmm1WeightScale,
+        gmm2Weight, gmm2WeightScale, expertScales,
+        shareGmm1WeightOptional, shareGmm1WeightScaleOptional,
+        shareGmm2WeightOptional, shareGmm2WeightScaleOptional,
+        expertSmoothScalesOptional, shareSmoothScalesOptional, xActiveMaskOptional,
+        groupEp, epRankSize, epRankId, moeExpertNum, quantMode, globalBs,
+        extInfo, shmemWorkspace,
+        output, shareOutput, expertTokenNums, workspaceSize, executor);
+}
+
+aclnnStatus aclnnFusedDeepMoe(
+    void *workspace,
+    uint64_t workspaceSize,
+    aclOpExecutor *executor,
+    aclrtStream stream)
+{
+    if (NnopbaseSetHcclServerType) {
+        if (op::GetCurrentPlatformInfo().GetSocVersion() == op::SocVersion::ASCEND910B) {
+            NnopbaseSetHcclServerType(executor, NNOPBASE_HCCL_SERVER_TYPE_AICPU);
+        } else {
+            NnopbaseSetHcclServerType(executor, NNOPBASE_HCCL_SERVER_TYPE_MTE);
+        }
+    }
+    return aclnnInnerFusedDeepMoe(workspace, workspaceSize, executor, stream);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/cam/comm_operator/ascend_kernels/fused_deep_moe/op_api/aclnn_fused_deep_moe.h
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Description: add fused deep moe interface header file.
+ * Create: 2025-07-21
+ * Note:
+ * History: 2025-07-21 add fused deep moe interface header file.
+ */
+
+#ifndef FUSED_DEEP_MOE
+#define FUSED_DEEP_MOE
+
+#include "aclnn/acl_meta.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__((visibility("default"))) aclnnStatus aclnnFusedDeepMoeGetWorkspaceSize(
+    const aclTensor *x,
+    const aclTensor *expertIds,
+    const aclTensorList *gmm1Weight,
+    const aclTensorList *gmm1WeightScale,
+    const aclTensorList *gmm2Weight,
+    const aclTensorList *gmm2WeightScale,
+    const aclTensor *expertScales,
+    const aclTensor *shareGmm1WeightOptional,
+    const aclTensor *shareGmm1WeightScaleOptional,
+    const aclTensor *shareGmm2WeightOptional,
+    const aclTensor *shareGmm2WeightScaleOptional,
+    const aclTensor *expertSmoothScalesOptional,
+    const aclTensor *shareSmoothScalesOptional,
+    const aclTensor *xActiveMaskOptional,
+    char *groupEp,
+    int64_t epRankSize,
+    int64_t epRankId,
+    int64_t moeExpertNum,
+    int64_t quantMode,
+    int64_t globalBs,
+    int64_t extInfo,
+    int64_t shmemWorkspace,
+    const aclTensor *output,
+    const aclTensor *shareOutput,
+    const aclTensor *expertTokenNums,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor);
+
+__attribute__((visibility("default"))) aclnnStatus aclnnFusedDeepMoe(
+    void *workspace,
+    uint64_t workspaceSize,
+    aclOpExecutor *executor,
+    aclrtStream stream);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
