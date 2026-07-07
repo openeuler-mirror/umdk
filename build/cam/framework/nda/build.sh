7d4c5bece05272401f1446f6d346847dce9ccc0c
simonkorl28 <machuan6@huawei.com>
2026-07-07 18:23:02 +0800

    cam: add nda build script

---
+++ build/cam/framework/nda/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+# Description: cam nda build script (download and build shmem package)
+# Create: 2026-07-07
+# Note:
+
+set -e
+
+export MODULE_NAME="nda"
+export MODULE_BUILD_OUT_PATH="${BUILD_OUT_PATH}/${MODULE_NAME}"
+
+# Validate soc_type: nda only supports ascend950
+if [[ -n "${SOC_TYPE}" && "${SOC_TYPE}" != "ascend950" ]]; then
+    echo "Error: nda module only supports soc_type 'ascend950', but got '${SOC_TYPE}'"
+    exit 1
+fi
+
+# rdma_backends source path (inside umdk)
+RDMA_BACKENDS_SRC="${ROOT_PATH}/src/cam/framework/nda/src/device/gm2gm/engine/rdma_backends"
+
+if [ ! -d "$MODULE_BUILD_OUT_PATH" ]; then
+    mkdir -p "$MODULE_BUILD_OUT_PATH"
+fi
+
+cd "$MODULE_BUILD_OUT_PATH"
+
+rm -rf shmem
+# Download and build the shmem package
+SHMEM_COMMIT_HASH="0b779694e5e8cf024a6e7b39efb724cbed2d3a1b"
+git clone https://gitcode.com/cann/shmem
+cd shmem
+git checkout "${SHMEM_COMMIT_HASH}"
+cd ..
+
+# Override the rdma_backends
+cp -f ${RDMA_BACKENDS_SRC}/* shmem/src/device/gm2gm/engine/rdma_backends/
+
+# Build shmem
+cd shmem
+EXTRA_OPTS=""
+if [[ "${BUILD_TYPE}" == "Debug" ]]; then
+    EXTRA_OPTS="-debug -enable_ascendc_dump"
+fi
+# Currently, the NDA function on Yunmai(xscale) network card only supports Ascend950
+bash scripts/build.sh -enable_rdma -rdma_backend XSCALE -soc_type Ascend950 ${EXTRA_OPTS}
