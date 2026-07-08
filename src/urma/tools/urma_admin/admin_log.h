28a1d84fc7520d27cecfb6210db65cd1a0a940f7
chenyutao <chenyutao2@huawei.com>
2026-07-08 09:47:38 +0800

    urma: fix log format for libumra_init, urma_admin, uvs
    Signed-off-by: chenyutao <chenyutao2@huawei.com>

--- src/urma/tools/urma_admin/admin_log.h
+++ src/urma/tools/urma_admin/admin_log.h
@@ -11,7 +11,11 @@
 #ifndef URMA_ADMIN_LOG_H
 #define URMA_ADMIN_LOG_H
 
-void urma_admin_log(const char *function, int line, const char *format, ...);
-#define URMA_ADMIN_LOG(...) urma_admin_log(__func__, __LINE__, __VA_ARGS__)
+#ifndef __FILE_NAME__
+#define __FILE_NAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
+void urma_admin_log(const char *file, const char *function, int line, const char *format, ...);
+#define URMA_ADMIN_LOG(...) urma_admin_log(__FILE_NAME__, __func__, __LINE__, __VA_ARGS__)
 
 #endif
