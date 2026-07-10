727934f6741c2d00b5a5ff6dafcfde061ca0ad6b
Bi Shulei <bishulei@huawei.com>
2026-07-10 17:21:21 +0800

    urma: support compact sge mode to reduce wr_buf memory
    (cherry picked from commit 3e7eeb7b7fdd2599ae9cf36ff0377af77313b022)

--- src/urma/lib/urma/bond/bondp_wr_buf.h
+++ src/urma/lib/urma/bond/bondp_wr_buf.h
@@ -37,33 +37,54 @@ typedef struct wr_buf_entry_hdr {
     uint8_t entry_type;
 } wr_buf_entry_hdr_t;
 
+/*
+ * sge_data is a flexible array member at the end of the entry.
+ * For jfs: [0 .. max_sge-1] = src_sge, [max_sge .. 2*max_sge-1] = dst_sge.
+ * For jfr: [0 .. max_sge-1] = src_sge.
+ * The actual entry size is computed at wr_buf_init time as:
+ *   sizeof(header) + <num_sge_arrays> * max_sge * sizeof(urma_sge_t)
+ */
 typedef struct jfs_wr_entry {
     uint64_t wr_id;
     uint8_t entry_type;
     urma_jfs_wr_t wr;
-    urma_sge_t src_sge[BONDP_MAX_SGE_NUM];
-    urma_sge_t dst_sge[BONDP_MAX_SGE_NUM];
     uint64_t user_ctx;
     struct bondp_comp *bdp_comp;
     struct bondp_target_jetty *target_vjetty;
     uint32_t send_idx;
     uint32_t target_idx;
+    urma_sge_t sge_data[];
 } jfs_wr_entry_t;
 
 typedef struct jfr_wr_entry {
     uint64_t wr_id;
     uint8_t entry_type;
     urma_jfr_wr_t wr;
-    urma_sge_t src_sge[BONDP_MAX_SGE_NUM];
     uint64_t user_ctx;
     struct bondp_comp *bdp_comp;
     uint32_t recv_idx;
+    urma_sge_t sge_data[];
 } jfr_wr_entry_t;
+
+/* sge access helpers */
+static inline urma_sge_t *jfs_wr_entry_src_sge(jfs_wr_entry_t *e)
+{
+    return e->sge_data;
+}
+static inline urma_sge_t *jfs_wr_entry_dst_sge(jfs_wr_entry_t *e, uint32_t max_sge)
+{
+    return e->sge_data + max_sge;
+}
+static inline urma_sge_t *jfr_wr_entry_src_sge(jfr_wr_entry_t *e)
+{
+    return e->sge_data;
+}
 
 typedef struct wr_buf {
     uint32_t max_wr_num;
     uint32_t latest_used;       /* deprecated: only used by WR_BUF_FOREACH, no longer updated */
     uint32_t wr_entry_size;
+    uint32_t max_sge;           /* max sge count per sge array in each entry */
     void *entries;
     uint32_t free_head;         /* index of first free entry, UINT32_MAX = empty */
     uint32_t *next_free;        /* next_free[idx] = next free entry index, UINT32_MAX = end */
@@ -99,7 +120,7 @@ static inline uint32_t wr_buf_idx_from_ptr(wr_buf_t *buf, char *ptr)
 #define JFR_WR_BUF_FOREACH(buf, idx_var, entry_var) \
     WR_BUF_FOREACH((buf), jfr_wr_entry_t, idx_var, entry_var)
 
-int wr_buf_init(wr_buf_t *buf, uint32_t max_wr_num);
+int wr_buf_init(wr_buf_t *buf, uint32_t max_wr_num, uint32_t max_sge);
 void wr_buf_uninit(wr_buf_t *buf);
 
 static inline jfs_wr_entry_t *jfs_wr_buf_get(wr_buf_t *buf, uint64_t wr_id)
