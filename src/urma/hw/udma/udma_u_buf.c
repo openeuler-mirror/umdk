shop-pin/urma_learning

commit a7ea7e683ec655f1c9b53540e9a51203c46eb0a2

udma: support alloc and free tid
category: feature
bugzilla: https://gitee.com/openeuler/umdk/issues/ID45DE
CVE: NA

-----------------------------------------------------------

TID (Token ID): Identifies the address space and permission
space to which resources in the UB system belong. Before
registering an application with Segment, you first need
to apply for a tid.

Signed-off-by: Jiahao Zhang <zhangjiahao14@huawei.com>

src/urma/hw/udma/udma_u_common.h
@@ -146,6 +146,11 @@ struct udma_u_jfc {
 	uint32_t cq_shift;
 };
 
+struct udma_u_tid {
+	urma_token_id_t base;
+	uint32_t tid;
+};
+
 struct udma_u_jetty_grp {
 	urma_jetty_grp_t base;
 	uint32_t jetty_cnt;
@@ -309,6 +314,11 @@ static inline uint32_t align_power2(uint32_t n)
 		res++;
 
 	return res;
+}
+
+static inline struct udma_u_tid *to_udma_u_tid(urma_token_id_t *key_id)
+{
+	return container_of(key_id, struct udma_u_tid, base);
 }
 
 static inline struct udma_u_jetty_grp *

src/urma/hw/udma/udma_u_ops.c
@@ -14,6 +14,7 @@
 #include "udma_u_jfs.h"
 #include "udma_u_jfr.h"
 #include "udma_u_jetty.h"
+#include "udma_u_tid.h"
 #include "udma_u_db.h"
 #include "udma_u_ctrlq_tp.h"
 #include "udma_u_ops.h"
@@ -52,6 +53,8 @@ static urma_ops_t g_udma_ops = {
 	.import_jetty_ex = udma_u_import_jetty_ex,
 	.import_jfr_ex = udma_u_import_jfr_ex,
 	.bind_jetty_ex = udma_u_bind_jetty_ex,
+	.alloc_token_id = udma_u_alloc_tid,
+	.free_token_id = udma_u_free_tid,
 	.get_async_event = udma_u_get_async_event,
 	.ack_async_event = udma_u_ack_async_event,
 	.post_jfs_wr = udma_u_post_jfs_wr,

src/urma/hw/udma/CMakeLists.txt
@@ -14,6 +14,7 @@ target_include_directories(BETA_UDMA_SO PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/kern
 
 target_link_libraries(BETA_UDMA_SO PRIVATE urma)
 target_link_libraries(BETA_UDMA_SO PRIVATE urma_common)
+target_link_libraries(BETA_UDMA_SO PRIVATE ummu)
 
 set(UDMA_NAME urma-udma)
 set_target_properties(BETA_UDMA_SO PROPERTIES OUTPUT_NAME ${UDMA_NAME})

src/urma/hw/udma/udma_u_tid.c
@@ -1,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#include <ummu_api.h>
+#include "udma_u_tid.h"
+
+static int udma_exec_alloc_tid_cmd(urma_context_t *ctx, uint32_t tid,
+				   urma_token_id_t *keyid)
+{
+	urma_cmd_udrv_priv_t udata = {};
+	int ret;
+
+	udma_u_set_udata(&udata, &tid, sizeof(tid), NULL, 0);
+	ret = urma_cmd_alloc_token_id(ctx, keyid, &udata);
+	if (ret)
+		UDMA_LOG_ERR("urma cmd alloc tid failed, ret = %d.\n", ret);
+
+	return ret;
+}
+
+static urma_token_id_t *udma_u_alloc_tid_common(urma_context_t *ctx, enum ummu_mapt_mode mapt_mode)
+{
+	struct ummu_tid_attr tid_attr = {.mode = mapt_mode};
+	struct udma_u_tid *udma_tid;
+	uint32_t tid;
+	int ret;
+
+	udma_tid = (struct udma_u_tid *)calloc(1, sizeof(*udma_tid));
+	if (udma_tid == NULL) {
+		UDMA_LOG_ERR("allocate udma tid failed.\n");
+		return NULL;
+	}
+
+	ret = ummu_allocate_tid(&tid_attr, &tid);
+	if (ret != 0) {
+		UDMA_LOG_ERR("ummu allocate tid failed, ret = %d.\n", ret);
+		goto err_ummu_alloc_tid;
+	}
+
+	if (tid > UDMA_MAX_TID) {
+		UDMA_LOG_ERR("ummu allocate tid overflow.\n");
+		goto err_cmd_alloc_key_id;
+	}
+
+	udma_tid->tid = tid;
+	udma_tid->base.token_id = tid << UDMA_TID_SHIFT;
+	ret = udma_exec_alloc_tid_cmd(ctx, udma_tid->base.token_id, &udma_tid->base);
+	if (ret != 0)
+		goto err_cmd_alloc_key_id;
+
+	udma_tid->base.urma_ctx = ctx;
+
+	return &udma_tid->base;
+
+err_cmd_alloc_key_id:
+	ret = ummu_free_tid(tid);
+	if (ret != 0)
+		UDMA_LOG_ERR("ummu free tid failed, ret = %d.\n", ret);
+err_ummu_alloc_tid:
+	free(udma_tid);
+	return NULL;
+}
+
+urma_status_t udma_u_free_tid(urma_token_id_t *tid)
+{
+	struct udma_u_tid *udma_tid = to_udma_u_tid(tid);
+	int ret;
+
+	ret = urma_cmd_free_token_id(tid);
+	if (ret != 0) {
+		UDMA_LOG_ERR("urma cmd free tid failed, ret = %d.\n", ret);
+		return URMA_FAIL;
+	}
+
+	ret = ummu_free_tid(udma_tid->tid);
+	if (ret != 0)
+		UDMA_LOG_ERR("ummu free tid failed, ret = %d.\n", ret);
+
+	free(udma_tid);
+
+	return URMA_SUCCESS;
+}
+
+urma_token_id_t *udma_u_alloc_tid(urma_context_t *ctx)
+{
+	return udma_u_alloc_tid_common(ctx, MAPT_MODE_TABLE);
+}

src/urma/hw/udma/udma_u_tid.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#ifndef __UDMA_U_TID_H__
+#define __UDMA_U_TID_H__
+
+#include "udma_u_common.h"
+
+urma_token_id_t *udma_u_alloc_tid(urma_context_t *ctx);
+urma_status_t udma_u_free_tid(urma_token_id_t *tid);
+
+#endif /* __UDMA_U_TID_H__ */

openeuler-mirror/umdk

commit e0a3de7bc12194c2418f3e4ca6b65025667dbbbf

udma: bugfix related to dtu_en
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/umdk/issues/692
CVE: NA

-----------------------------------------------------------

This patch fix a bug related to dtu_en.

Fixes: a7ea7e683ec6 ("udma: support alloc and free tid")

Signed-off-by: Wei Qin <qinwei61@huawei.com>

src/urma/hw/udma/udma_u_buf.c
@@ -61,7 +61,7 @@ bool udma_u_alloc_queue_buf(struct udma_u_jetty_queue *q, uint32_t max_entry_cnt
 
 	if (q->dtu_en || q->sq_reserved)
 		return true;
-	if (!q->cstm && q->ctx->hugepage_enable) {
+	if (q->ctx->hugepage_enable) {
 		q->hugepage = udma_u_alloc_hugepage(q->ctx, q->qbuf_size);
 		if (q->hugepage) {
 			q->qbuf = q->hugepage->va_start;
@@ -69,7 +69,7 @@ bool udma_u_alloc_queue_buf(struct udma_u_jetty_queue *q, uint32_t max_entry_cnt
 			UDMA_LOG_WARN("hugepage buffer unavailable, fallback to normal buffer.\n");
 			q->qbuf = udma_u_alloc_kernel_buf(q->ctx, q->qbuf_size);
 		}
-	} else if (!q->cstm) {
+	} else {
 		q->qbuf = udma_u_alloc_kernel_buf(q->ctx, q->qbuf_size);
 	}
 
@@ -95,7 +95,7 @@ void udma_u_free_queue_buf(struct udma_u_jetty_queue *q)
 		q->wrid = NULL;
 	}
 
-	if (q->dtu_en || q->cstm || q->sq_reserved)
+	if (q->cstm || q->dtu_en || q->sq_reserved)
 		return;
 
 	if (q->qbuf != NULL) {

src/urma/hw/udma/udma_u_common.h
@@ -81,8 +81,9 @@ struct udma_u_hugepage {
 	struct udma_u_hugepage_priv *priv;
 };
 
-typedef int (*dtu_u_mmap_func)(uint64_t va_base, uint64_t va_size, void **dtu_va);
-typedef void (*dtu_u_munmap_func)(void **g_dtu_va, uint64_t va_size);
+typedef int (*dtu_u_mmap_func)(uint64_t va_base, uint64_t va_size, void **dtu_va,
+		uint32_t *dtu_va_refcount);
+typedef void (*dtu_u_munmap_func)(void **g_dtu_va, uint64_t va_size, uint32_t *dtu_va_refcount);
 
 /* 32 */
 #define UDMA_JETTY_TABLE_NUM 1 << 5

src/urma/hw/udma/udma_u_jetty.c
@@ -1141,8 +1141,8 @@ urma_status_t udma_u_active_jetty(urma_jetty_t *jetty)
 	urma_cmd_udrv_priv_t udata = {};
 	int ret;
 
-    udma_jetty->sq.is_wqe_lock_buf_jetty =
-		is_support_wqe_lock_buffer_jetty(udma_jetty->sq.ctx, jetty->jetty_cfg.id, true);
+	udma_jetty->sq.is_wqe_lock_buf_jetty =
+		is_support_wqe_lock_buffer_jetty(udma_jetty->sq.ctx, jetty->jetty_cfg.id);
 	if (udma_jetty->sq.is_wqe_lock_buf_jetty) {
 		if (udma_u_check_wqe_lock_buffer_jetty(udma_jetty))
 			return URMA_EINVAL;

src/urma/hw/udma/udma_u_jfs.c
@@ -991,7 +991,7 @@ urma_status_t udma_u_post_one_wr(struct udma_u_context *udma_ctx,
 
 	if (udma_check_sge_num_and_opcode(wr->opcode, sq, wr, &wqe_info.opcode,
 					  udma_ctx->atomic_add_en)) {
-		UDMA_LOG_ERR("WR SGE number or opcode is invalid.\n");
+		UDMA_LOG_ERR("WR SGE number or opcode: %u is invalid.\n", wr->opcode);
 		return URMA_EINVAL;
 	}
 

src/urma/hw/udma/udma_u_jfs.h
@@ -178,14 +178,14 @@ static inline uint32_t get_max_sge_num(uint8_t max_sge, uint32_t max_inline_size
 }
 
 static inline bool is_support_wqe_lock_buffer_jetty(struct udma_u_context *udma_ctx,
-						    uint32_t id, bool is_jetty)
+						    uint32_t id)
 {
 	bool is_ccu_jetty;
 
 	is_ccu_jetty = udma_ctx->ccu_jetty_max_cnt &&
 		       id >= udma_ctx->ccu_jetty_start_id &&
 		       id < (udma_ctx->ccu_jetty_start_id + udma_ctx->ccu_jetty_max_cnt);
-	return is_jetty && is_ccu_jetty && udma_ctx->lock_buffer_en;
+	return is_ccu_jetty && udma_ctx->lock_buffer_en;
 }
 
 urma_status_t udma_u_post_sq_wr(struct udma_u_context *udma_ctx,

src/urma/hw/udma/udma_u_ops.c
@@ -27,6 +27,7 @@
 
 #define LIBDTU_FILE "/usr/lib64/libdtu.so"
 static void *g_dtu_va = NULL;
+static uint32_t g_dtu_va_refcount = 0;
 static pthread_mutex_t g_sq_reserved_lock = PTHREAD_MUTEX_INITIALIZER;
 static uint64_t g_sq_reserved_refcount = 0;
 static void *g_sq_reserved_va = NULL;
@@ -255,12 +256,8 @@ err_mmap_dlsym:
 
 static void udma_close_dtu_provider(struct udma_u_context *udma_ctx)
 {
-	if (udma_ctx->dtu_munmap_fun_ptr) {
-		udma_ctx->dtu_munmap_fun_ptr(&g_dtu_va, udma_ctx->dtu_va_size);
-		udma_ctx->dtu_munmap_fun_ptr = NULL;
-	}
+	udma_ctx->dtu_munmap_fun_ptr = NULL;
 	udma_ctx->dtu_mmap_fun_ptr = NULL;
-	udma_ctx->dtu_enable = false;
 	dlclose(udma_ctx->dtu_provider_handle);
 }
 
@@ -304,9 +301,10 @@ static urma_context_t *udma_u_create_context(urma_device_t *dev, uint32_t eid_in
 	udma_u_init_context(udma_ctx, &resp);
 	if (udma_ctx->dtu_enable) {
 		if (udma_open_dtu_provider(udma_ctx)) {
-			if (udma_ctx->dtu_mmap_fun_ptr(udma_ctx->dtu_va_base, udma_ctx->dtu_va_size, &g_dtu_va)) {
-				udma_ctx->dtu_mmap_fun_ptr = NULL;
+			if (udma_ctx->dtu_mmap_fun_ptr(udma_ctx->dtu_va_base,
+			    udma_ctx->dtu_va_size, &g_dtu_va, &g_dtu_va_refcount)) {
 				udma_close_dtu_provider(udma_ctx);
+				udma_ctx->dtu_enable = false;
 				UDMA_LOG_WARN("Could not mmap dtu, continuing with the normal process.\n");
 			}
 		} else {
@@ -330,8 +328,10 @@ static urma_context_t *udma_u_create_context(urma_device_t *dev, uint32_t eid_in
 err_reserved_sq:
 	udma_u_free_db(&udma_ctx->urma_ctx, &udma_ctx->db);
 err_alloc_db:
-	if (udma_ctx->dtu_enable)
+	if (udma_ctx->dtu_enable) {
+		udma_ctx->dtu_munmap_fun_ptr(&g_dtu_va, udma_ctx->dtu_va_size, &g_dtu_va_refcount);
 		udma_close_dtu_provider(udma_ctx);
+	}
 	(void)urma_cmd_delete_context(&udma_ctx->urma_ctx);
 err_create_ctx:
 	(void)pthread_mutex_destroy(&udma_ctx->hugepage_lock);
@@ -351,9 +351,10 @@ static urma_status_t udma_u_delete_context(urma_context_t *ctx)
 	udma_u_destroy_jt_table(udma_ctx);
 	udma_u_free_reserved_sq();
 	udma_u_free_db(ctx, &udma_ctx->db);
-	if (udma_ctx->dtu_enable)
+	if (udma_ctx->dtu_enable) {
+		udma_ctx->dtu_munmap_fun_ptr(&g_dtu_va, udma_ctx->dtu_va_size, &g_dtu_va_refcount);
 		udma_close_dtu_provider(udma_ctx);
-
+	}
 	udma_u_destroy_hugepage(udma_ctx);
 
 	if (urma_cmd_delete_context(&udma_ctx->urma_ctx)) {