060c377c7dfb78b96ae6cadf0a16857b06523fc3
x1314aq <x1314aq@icloud.com>
2026-07-25 14:31:16 +0800

    fix build issue

--- src/cam/comm_operator/pybind/quant_lightning_indexer.cpp
+++ src/cam/comm_operator/pybind/quant_lightning_indexer.cpp
@@ -1,4 +1,4 @@
-2/**
+/**
  * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
