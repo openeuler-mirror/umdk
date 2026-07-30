openeuler-mirror/umdk

commit 82ba716f9ca7a48f2f1826245525a85b84680478

urma: name bond worker thread
Set the bond background worker thread name to urma_bond_worker so that it can be identified in system thread listings and diagnostics.

Fixes: d31b69d5d4d9 ("urma: add background worker thread for bond device")

Signed-off-by: luyizhou <luyizhou1@huawei.com>

src/urma/lib/urma/bond/utils/bondp_worker.c
@@ -4,6 +4,8 @@
  * Description: Bond worker thread implementation
  */
 
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
@@ -25,6 +27,7 @@
 #define BONDP_WORKER_EVENT_NUM         (16)
 #define BONDP_WORKER_HANDLER_MAP_SIZE  (64U)
 #define BONDP_WORKER_MAX_ADVANCE_TICKS (64U)
+#define BONDP_WORKER_THREAD_NAME       "urma_bond_worker"
 
 typedef struct bondp_worker_event_handler {
     int fd;
@@ -418,6 +421,10 @@ static bondp_worker_t *bondp_worker_create_instance(int *err_code)
         URMA_LOG_ERR("Failed to create bond worker thread, ret: %d.\n", ret);
         *err_code = -ret;
         goto ERR_WAKE;
+    }
+    ret = pthread_setname_np(worker->thread, BONDP_WORKER_THREAD_NAME);
+    if (ret != 0) {
+        URMA_LOG_WARN("Failed to set bond worker thread name, ret: %d.\n", ret);
     }
 
     return worker;

commit 070d7f42c3d35d2fcb2b17b4d4c22a25d706c5b7

urma: shorten bond worker thread name
pthread_setname_np allows at most 15 visible characters, but the urma_bond_worker name contains 16 and causes ERANGE.

Shorten the name to urma_bond_wrk so that the worker thread can be identified in system diagnostics.

Fixes: 82ba716f9ca7 ("urma: name bond worker thread")
Signed-off-by: luyizhou <luyizhou1@huawei.com>

src/urma/lib/urma/bond/utils/bondp_worker.c
@@ -27,7 +27,7 @@
 #define BONDP_WORKER_EVENT_NUM         (16)
 #define BONDP_WORKER_HANDLER_MAP_SIZE  (64U)
 #define BONDP_WORKER_MAX_ADVANCE_TICKS (64U)
-#define BONDP_WORKER_THREAD_NAME       "urma_bond_worker"
+#define BONDP_WORKER_THREAD_NAME       "urma_bond_wrk"
 
 typedef struct bondp_worker_event_handler {
     int fd;