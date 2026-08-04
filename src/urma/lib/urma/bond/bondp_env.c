c8ed40e297000905144d54793c766a8615574abc
wanghang73 <wanghang73@huawei.com>
2026-08-04 15:05:47 +0800

    urma: retry bond sends on RNR retry exhaustion

--- src/urma/lib/urma/bond/bondp_env.c
+++ src/urma/lib/urma/bond/bondp_env.c
@@ -20,6 +20,8 @@
 #define BONDP_ENV_ENABLE_HEALTH_CHECK   "BOND_ENABLE_HEALTH_CHECK"
 #define BONDP_ENV_HEALTH_CHECK_INTERVAL "BOND_HEALTH_CHECK_INTERVAL"
 #define BONDP_ENV_HEALTH_CHECK_NODE_NUM "BOND_HEALTH_CHECK_BATCH_NODE_NUM"
+#define BONDP_ENV_RNR_RETRY_SLEEP_MS    "BOND_RNR_RETRY_SLEEP_MS"
+#define BONDP_ENV_RNR_RETRY_MAX         "BOND_RNR_RETRY_MAX"
 #define BONDP_ENV_LEN_MAX               (128)
 /*
  * #define BONDP_ENV_FAILOVER_DIEX_Y_ROUTEZ          "BOND_FAILOVER_DIEX_Y_ROUTEZ"
@@ -141,6 +143,8 @@ static void read_all_env(bondp_env_t *env)
     const bool default_enable_failback = true;
     const bool default_enable_failover = true;
     const uint64_t default_health_check_interval_ms = BONDP_HC_DEFAULT_PROBE_INTERVAL_MS;
+    const uint64_t default_rnr_retry_sleep_ms = 0;
+    const uint64_t default_rnr_retry_max = 7;
     const uint32_t default_health_check_batch_node_num = BONDP_HC_DEFAULT_BATCH_NODE_NUM;
     env->enable_health_check = read_env_bool(
         BONDP_ENV_ENABLE_HEALTH_CHECK, default_enable_health_check);
@@ -150,6 +154,10 @@ static void read_all_env(bondp_env_t *env)
         BONDP_ENV_ENABLE_FAILBACK, default_enable_failback);
     env->health_check_interval_ms = read_env_uint64(
         BONDP_ENV_HEALTH_CHECK_INTERVAL, default_health_check_interval_ms);
+    env->rnr_retry_sleep_ms = read_env_uint64(
+        BONDP_ENV_RNR_RETRY_SLEEP_MS, default_rnr_retry_sleep_ms);
+    env->rnr_retry_max = read_env_uint64(
+        BONDP_ENV_RNR_RETRY_MAX, default_rnr_retry_max);
     uint64_t health_check_batch_node_num = read_env_uint64(
         BONDP_ENV_HEALTH_CHECK_NODE_NUM, default_health_check_batch_node_num);
     env->health_check_batch_node_num = default_health_check_batch_node_num;
@@ -175,12 +183,14 @@ static void read_all_env(bondp_env_t *env)
 static void print_all_env(const bondp_env_t *env)
 {
     URMA_LOG_INFO("Health check config: enable_failover=%s, enable_failback=%s, enable_health_check=%s, "
-                  "interval=%lums, batch_node_num=%u\n",
+                  "interval=%lums, batch_node_num=%u, rnr_retry_sleep=%lums, rnr_retry_max=%lu\n",
                   env->enable_failover ? "true" : "false",
                   env->enable_failback ? "true" : "false",
                   env->enable_health_check ? "true" : "false",
                   env->health_check_interval_ms,
-                  env->health_check_batch_node_num);
+                  env->health_check_batch_node_num,
+                  env->rnr_retry_sleep_ms,
+                  env->rnr_retry_max);
 }
 
 static void init_path(bondp_env_t *env)
