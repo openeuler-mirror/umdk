d49d58d23f191c79b28de33cdcf06d497655cd7b
Bi Shulei <bishulei@huawei.com>
2026-08-06 21:19:34 +0800

    urma: revert shrink sge to [2] per WR
    (cherry picked from commit b8356dbdb8fd210c6e3582446ad6262679ea1fe3)

--- src/urma/lib/urma/bond/bondp_wr_buf.h
+++ src/urma/lib/urma/bond/bondp_wr_buf.h
@@ -16,7 +16,7 @@
 
 #include "urma_types.h"
 
-#define BONDP_MAX_SGE_NUM             (1)
+#define BONDP_MAX_SGE_NUM             (32)
 #define BONDP_BATCH_POST_MAX_NUM      (256)
 
 #ifdef __cplusplus
