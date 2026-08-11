5ee79c6d7f4e0c546b64063f6947134e91741d38
wx1831 <wangxin554@huawei.com>
2026-08-11 09:11:55 +0800

    umq: fix code format

--- src/urpc/umq/qbuf/umq_tiny_qbuf_pool.h
+++ src/urpc/umq/qbuf/umq_tiny_qbuf_pool.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
  * Description: define tiny qbuf pool function
  * Create: 2026-5-28
  */
