fc1437ad7a71cc2224e6ed731c2dcebd62de927f
wx1831 <wangxin554@huawei.com>
2026-08-31 21:34:07 +0800

    ﻿umq: add queue info dfx
    (cherry picked from commit b44f75cc8c0b16ddb3583d3fd8cb640a0bea0094)

--- src/urpc/umq/umq_ub/core/umq_ub_impl.c
+++ src/urpc/umq/umq_ub/core/umq_ub_impl.c
@@ -2295,7 +2295,7 @@ int umq_ub_unbind_impl(uint64_t umqh)
     } else {
         queue->bind_ctx = NULL;
     }
-    (void)umq_ub_remote_tseg_info_release(qcfg->dev_ctx->remote_imported_info, bind_ctx);
+    (void)umq_ub_remote_tseg_info_release(queue->dev_ctx->remote_imported_info, bind_ctx);
     UMQ_VLOG_DEBUG(VLOG_UMQ, "UMQ(ID:%u), remote eid: " EID_FMT ", remote jetty_id: %u, unbind jetty\n", queue->umq_id,
         EID_ARGS(tjetty->id.eid), tjetty->id.id);
     if (qcfg->tp_mode == URMA_TM_RC) {
